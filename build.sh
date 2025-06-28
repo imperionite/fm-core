4c5bcccbd547be950605e243a9a0fb7f7ed86523
imperionite <lr.aimperial@mmdc.mcl.edu.ph>
2025-06-28 17:44:27 +0800

    :rocket: (.sh) edit file

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 # Exit on error
 set -o errexit
 
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+# Collect static files (needed for Django Admin and any other static assets)
+python manage.py collectstatic --noinput
+
+
 # Run makemigrations dry-run to detect errors or issues
 echo "Checking for migration issues (dry run)..."
 if ! python manage.py makemigrations --dry-run --verbosity 3 --check; then
@@ -16,5 +23,3 @@ python manage.py migrate --no-input
 # Run the custom command to create super user, staff & seed initial regular users
 python manage.py seed_initial_users
 
-# Collect static files
-python manage.py collectstatic --noinput
